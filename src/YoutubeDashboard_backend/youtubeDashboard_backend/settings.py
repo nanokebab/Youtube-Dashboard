5ad172068cd77f40f89d45abab3ad6e1fd880d8f
Sven <39420314+nanokebab@users.noreply.github.com>
2024-10-14 01:39:14 +0200

    moved some files around

--- src/YoutubeDashboard_backend/youtubeDashboard_backend/settings.py
+++ src/YoutubeDashboard_backend/youtubeDashboard_backend/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'youtubeDashboard_backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': '/Users/derkwild/Documents/GitHub/Youtube-Dashboard/src/subscribed_videos2.sqlite3',
+        'NAME': '/Users/derkwild/Documents/GitHub/Youtube-Dashboard/data/subscribed_videos2.sqlite3',
     }
     # 'NAME': BASE_DIR / 'db.sqlite3',
     # ,
