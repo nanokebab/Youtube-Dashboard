0678dc3773e4a06abe895077fdc638fa50afb3a5
Sven <39420314+nanokebab@users.noreply.github.com>
2024-10-14 15:25:08 +0200

    changed filestructure

--- src/django/youtubeDashboard_backend/settings.py
+++ src/django/youtubeDashboard_backend/settings.py
@@ -77,8 +77,10 @@ WSGI_APPLICATION = 'youtubeDashboard_backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'subscribed_videos.sqlite3'
+        'NAME': BASE_DIR / 'subscribed_videos.sqlite3',
     }
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
